92ec8768461e836a40d3c85cd19194438161c88c
Pedro Henriquee <pedroh.robotics@gmail.com>
2026-03-26 16:03:09 -0300

    Exercício 01 realizado

---
+++ alocacao_de_memoria/Exercicio_01.c
@@ -0,0 +1,40 @@
+/*Implemente a funçÃO preenche, esboçada abaixo. A função deve preencher os elementos de um vetor v de
+tamanho n com um certo valor x. Para acessar os elementos do vetor, não utilize índices, use apenas aritmética
+de ponteiros.*/
+
+// *v = endereço do primeiro elemento do vetor "v"
+// n = tamanho do vetor "v"
+// x = valor a inserir em todas as posições do vetor "v"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void preenche (int *
+v, int n, int x){
+    int *p = v; // p aponta para o primeiro elemento do vetor
+    for (int i = 0; i < n; i++) {
+        *p = x; // atribui o valor x ao elemento apontado por p
+        p++; // avança para o próximo elemento
+    }
+}
+int main()
+{
+    
+    int tamanho, valor, *p;
+    
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &tamanho);
+    printf("Informe o valor a preencher o vetor: ");
+    scanf("%d", &valor);
+
+    p = malloc(tamanho * sizeof(int));
+
+    preenche(p, tamanho, valor);
+
+    printf("Vetor preenchido: ");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d ", p[i]);
+    }
+    printf("\n");
+
+}
---
+++ alocacao_de_memoria/Exercicio_02.c
@@ -0,0 +1,12 @@
+/*Escreva um programa em linguagem C que solicita ao usuário a quantidade de alunos de uma turma e aloca um
+vetor para armazenar a nota semestral de cada um dos alunos (tipo float). Depois de ler as notas e armazena-las
+no vetor, apresente a média aritmética das notas da turma. O vetor deve ser alocado dinamicamente na
+memória.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* code */
+    return 0;
+}
